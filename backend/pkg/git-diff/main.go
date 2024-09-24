6778907437a6945ef0b38e3c18ad9712d69351ea
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2024-09-24 06:02:13 +0200

    chore: update

---
+++ backend/pkg/git-diff/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/sourcegraph/conc/iter"
+	"github.com/sourcegraph/go-diff/diff"
+)
+
+var links = []string{
+	"https://github.com/WojciechMatuszewski/programming-notes/commit/faeae7964e4b3903f5bcbd139c2613f134ff8865.diff",
+	"https://github.com/WojciechMatuszewski/programming-notes/commit/5c53523cac0003708d96dcafb420ce918d608eb8.diff",
+	"https://github.com/WojciechMatuszewski/programming-notes/commit/ce7eb2fc7c0ad25bc0b95213fa32274a533bc053.diff",
+}
+
+func main() {
+	diffs := iter.Map(links, func(url *string) string {
+		return fetchDiff(context.Background(), *url)
+	})
+
+	content := strings.TrimSpace(strings.Join(diffs, "\n"))
+	fmt.Println(content)
+}
+
+func fetchDiff(ctx context.Context, url string) string {
+	client := &http.Client{}
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	reader := diff.NewMultiFileDiffReader(resp.Body)
+	gitDiff, err := reader.ReadAllFiles()
+	if err != nil {
+		panic(err)
+	}
+
+	var content []string
+	for _, fileDiff := range gitDiff {
+		for _, diffChunk := range fileDiff.Hunks {
+			content = append(content, string(diffChunk.Body))
+		}
+	}
+
+	return strings.TrimSpace(strings.Join(content, "\n"))
+}
--- backend/pkg/go.mod
+++ backend/pkg/go.mod
@@ -1,3 +1,8 @@
 module pkg
 
 go 1.22.4
+
+require (
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/sourcegraph/go-diff v0.7.0 // indirect
+)
